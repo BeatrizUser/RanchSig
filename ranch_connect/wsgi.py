bab08d17fed8ff793915b135e9a2668fa754e102
BeatrizUser <beatriz.machadosilva@gmail.com>
2023-07-13 21:40:46 -0300

    fix4

--- ranch_connect/wsgi.py
+++ ranch_connect/wsgi.py
@@ -9,17 +9,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 import sys
-
 from django.core.wsgi import get_wsgi_application
 
-# Obtenha o diretório raiz do projeto
-root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Adicione o diretório raiz do projeto ao sys.path
+root_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root_path)
 
-# Defina a variável de ambiente DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranch_connect.settings')
 
-# Obtenha a aplicação WSGI do Django
 application = get_wsgi_application()
+
