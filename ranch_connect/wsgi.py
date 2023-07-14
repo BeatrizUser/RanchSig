3042764a7d20e52a91918d5b45d860f3409d0046
BeatrizUser <beatriz.machadosilva@gmail.com>
2023-07-13 21:24:10 -0300

    deploy pythonanywhere

--- ranch_connect/wsgi.py
+++ ranch_connect/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+# Adicione o diretório raiz do projeto ao sys.path
+sys.path.append('/home/btrz92/RanchSig')
+
+# Defina a variável de ambiente DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranch_connect.settings')
 
+# Obtenha a aplicação WSGI do Django
 application = get_wsgi_application()
